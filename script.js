33b89380692154d30044f1b9a2da2e0b59d73d92
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2025-02-02 22:49:24 +0530

    add better design banner

--- script.js
+++ script.js
@@ -2,16 +2,16 @@ let navLinks = document.getElementById("navLinks");
 let entireBody = document.getElementById("entireBody");
 let navbar = document.getElementById("navbar");
 
-function showMenu(){
-  navLinks.style.left="0";
-  entireBody.style.position="fixed";
-  navbar.style.height="100vh";
+function showMenu() {
+  navLinks.style.left = "0";
+  entireBody.style.position = "fixed";
+  navbar.style.height = "100vh";
 }
 
-function hideMenu(){
-  navLinks.style.left="-100%";
-  entireBody.style.position="static"; 
-  navbar.style.height="10vmin";
+function hideMenu() {
+  navLinks.style.left = "-100%";
+  entireBody.style.position = "static";
+  navbar.style.height = "10vmin";
 }
 
 function checkWindowSize() {
@@ -20,6 +20,6 @@ function checkWindowSize() {
   }
 }
 
-window.addEventListener('resize', checkWindowSize);
+window.addEventListener("resize", checkWindowSize);
 
 checkWindowSize();
