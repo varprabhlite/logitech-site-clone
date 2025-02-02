40d36a08fbd0cf7694ef7daef3bb2dabca3022ad
varprabhlite <185780179+varprabhlite@users.noreply.github.com>
2025-02-02 22:38:49 +0530

    Add design banner

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 let navLinks = document.getElementById("navLinks");
+let entireBody = document.getElementById("entireBody");
+let navbar = document.getElementById("navbar");
+
 function showMenu(){
   navLinks.style.left="0";
+  entireBody.style.position="fixed";
+  navbar.style.height="100vh";
 }
+
 function hideMenu(){
   navLinks.style.left="-100%";
+  entireBody.style.position="static"; 
+  navbar.style.height="10vmin";
 }
+
+function checkWindowSize() {
+  if (window.innerWidth >= 1024) {
+    navbar.style.height = "10vmin";
+  }
+}
+
+window.addEventListener('resize', checkWindowSize);
+
+checkWindowSize();
